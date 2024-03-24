d5fdaf6de619adfd108993f97943feaed757fa19
yuyijun <yuyijun@enn.cn>
2024-03-24 23:43:19 +0800

    解析excel 结果 并封装返回展示

--- src/main/java/com/fish/vwhub/service/impl/VwHubOutputService.java
+++ src/main/java/com/fish/vwhub/service/impl/VwHubOutputService.java
@@ -8,13 +8,13 @@ import com.fish.vwhub.entity.ResResult;
 import com.fish.vwhub.entity.VwHubOutput;
 import com.fish.vwhub.mapper.VwHubOutputMapper;
 import com.fish.vwhub.util.ExcelUtil;
+import com.fish.vwhub.util.GeoUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author fish
@@ -39,14 +39,42 @@ public class VwHubOutputService extends ServiceImpl<VwHubOutputMapper, VwHubOutp
             log.error("Excel读取失败，e:{0}", e);
             return ResResult.fail("500", "Excel文件读取失败，请重试");
         }
-        parseExcelData(jsonObjects);
-        return ResResult.success(null);
+        JSONObject jo = parseExcelData(jsonObjects);
+        return ResResult.success(jo);
     }
 
-    public void parseExcelData(List<JSONObject> dataList) {
-        JSONArray res = new JSONArray();
+    public JSONObject parseExcelData(List<JSONObject> dataList) {
+        JSONObject res = new JSONObject();
+        JSONArray pointRes = new JSONArray();
+        JSONArray lineRes = new JSONArray();
         for (JSONObject jo : dataList) {
-            //todo 解析excel 结果
+            String from = jo.getString("中转库城市");
+            String to = jo.getString("覆盖城市");
+            Float[] fromPoint = GeoUtil.getGeo(from);
+            Float[] toPoint = GeoUtil.getGeo(to);
+            if (fromPoint == null || toPoint == null) {
+                continue;
+            }
+            //点位集合 用于涟漪效果
+            JSONObject fromPointJo = new JSONObject();
+            fromPointJo.put("name", from);
+            fromPointJo.put("value", fromPoint);
+            JSONObject toPointJo = new JSONObject();
+            toPointJo.put("name", to);
+            toPointJo.put("value", toPoint);
+            pointRes.add(fromPointJo);
+            pointRes.add(toPointJo);
+
+            //飞线结果集合
+            JSONObject lineJo = new JSONObject();
+            JSONArray coords = new JSONArray();
+            coords.add(fromPoint);
+            coords.add(toPoint);
+            lineJo.put("coords", coords);
+            lineRes.add(lineJo);
         }
+        res.put("pointRes", pointRes);
+        res.put("lineRes", lineRes);
+        return res;
     }
 }
